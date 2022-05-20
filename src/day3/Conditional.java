9cce25bf923d17aa863eb8d0941fc83b4c0865f7
Vitor Melo <vitormelo9913@gmail.com>
2022-05-20 17:00:22 -0300

    Day 3

---
+++ src/day3/Conditional.java
@@ -0,0 +1,25 @@
+package day3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Conditional {
+	public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(bufferedReader.readLine().trim());
+
+        bufferedReader.close();
+
+        if (N % 2 == 1) {
+            System.out.println("Weird");
+        } else if (N % 2 == 0 && N >= 2 && N <= 5) {
+            System.out.println("Not Weird");
+        } else if (N % 2 == 0 && N >= 6 && N <= 20) {
+            System.out.println("Weird");
+        } else if (N % 2 == 0 && N > 20) {
+            System.out.println("Not Weird");
+        }
+    }
+}
