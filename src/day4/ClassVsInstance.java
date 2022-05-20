42e424a22fb9cc715bd0e52e35fe5bda051d5b0b
Vitor Melo <vitormelo9913@gmail.com>
2022-05-20 17:18:06 -0300

    Day 4

---
+++ src/day4/ClassVsInstance.java
@@ -0,0 +1,22 @@
+package day4;
+
+import java.util.Scanner;
+
+public class ClassVsInstance {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        for (int i = 0; i < T; i++) {
+            int age = sc.nextInt();
+            Person p = new Person(age);
+            p.amIOld();
+            for (int j = 0; j < 3; j++) {
+                p.yearPasses();
+            }
+            p.amIOld();
+            System.out.println();
+        }
+        sc.close();
+    }
+
+}
