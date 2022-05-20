c8794743cded1a3031514faf077b5b6624ee240f
Vitor Melo <vitormelo9913@gmail.com>
2022-05-20 16:59:48 -0300

    Day 0

--- src/day0/HelloWorld.java
+++ src/day0/HelloWorld.java
@@ -1,10 +1,21 @@
 package day0;
 
+import java.util.Scanner;
+
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		/*
+         * Day 0: Hello, World!
+         * To complete this challenge, you must save a line of input from
+         * stdin to a variable, print Hello, World. on a single line, and
+         * finally print the value of your variable on a second line.
+         */
+        System.out.println("Hello, World.");
+        Scanner scanner = new Scanner(System.in);
+        String inputString = scanner.nextLine();
+        scanner.close();
+        System.out.println(inputString);
 	}
 
 }
