c96e83ffd58353201d3ea5ba0ec64c74503fb81c
Vitor Melo <vitormelo9913@gmail.com>
2022-05-21 23:18:17 -0300

    Day 5

---
+++ src/day5/Loops.java
@@ -0,0 +1,20 @@
+package day5;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Loops {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        bufferedReader.close();
+        
+        for(int i = 1; i <= 10; i++){
+            System.out.println(n + " x " + i + " = " + i * n);
+        }
+    }
+    
+}
